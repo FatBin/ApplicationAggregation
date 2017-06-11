0bdb1fc3380a9ab8ada50eca032582a973fc1d81
pyl14 <pyl14@software.nju.edu.cn>
2017-06-11 22:53:21 +0800

    servlet ok

--- ApplicationAggregation/src/main/java/controller/SearchServlet.java
+++ ApplicationAggregation/src/main/java/controller/SearchServlet.java
@@ -33,7 +33,8 @@ public class SearchServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		PrintWriter out= response.getWriter();
+		
+		//use AllXMLService in MiddlewareService
 		
 		
 		
